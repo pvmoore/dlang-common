6180358bfbc0edf9b2fa6cd2382ad9e8dc3b547d
pvmoore <1926576+pvmoore@users.noreply.github.com>
2021-10-31 16:31:57 +0000

    Add getOrAdd

--- src/common/utils/map_utils.d
+++ src/common/utils/map_utils.d
@@ -10,14 +10,24 @@ bool containsKey(M,V)(M map, V value) if(isAssociativeArray!M) {
  * Add or replace values in map with values in other.
  *  map1.add(map2);
  */
-void add(M)(M map, M other) if(isAssociativeArray!M) {
+void add(M)(ref M map, M other) if(isAssociativeArray!M) {
     foreach(k, v; other) {
         map[k] = v;
     }
 }
-void putIfAbsent(K,V)( V[K] map, K key, V value) {
+void putIfAbsent(K,V)(ref V[K] map, K key, V value) {
 	auto p = key in map;
 	if(!p) {
 		map[key] = value;
 	}
+}
+/**
+ * auto p = map.getOrAdd("default");
+ */
+V* getOrAdd(K,V)(ref V[K] map, K key, V addMe) {
+	auto p = key in map;
+	if(p) return p;
+
+	map[key] = addMe;
+	return key in map;
 }
