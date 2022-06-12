31d3500ed177c7f58529fdd8836e253c2fa73bdc
pvmoore <1926576+pvmoore@users.noreply.github.com>
2022-06-12 14:40:18 +0100

    Add matchesAny regex function

--- src/common/utils/regex_utils.d
+++ src/common/utils/regex_utils.d
@@ -8,3 +8,10 @@ bool matches(string str, Regex!char r) {
 bool matches(Regex!char r, string str) {
     return !matchFirst(str, r).empty;
 }
+
+bool matchesAny(string str, Regex!(char)[] regexes...) {
+    foreach(r; regexes) {
+        if(!matchFirst(str,r).empty) return true;
+    }
+    return false;
+}
